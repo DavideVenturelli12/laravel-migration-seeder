7c9dda4a4bdeb4271d99a68c367838816d45c402
Davide Venturelli <dventurelli90@gmail.com>
2022-05-24 00:47:15 +0200

    creazione databse treni

---
+++ migration/database/migrations/2022_05_23_224006_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->datetime('orario_di_partenza')->nullable($value = true);
+            $table->datetime('orario_di_arrivo')->nullable($value = true);
+            $table->string('codice_treno');
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
